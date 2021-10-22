388db848ced1946bca5d8793d957823806c54389
Henry Onu <onuhenry24@gmail.com>
2021-10-22 17:15:07 +0100

    Add algorithem for replacement

--- lib/play.rb
+++ lib/play.rb
@@ -10,6 +10,10 @@ class Hangman
         @live = 5
         @correct_guess = []
         @letters = ('a'..'z').to_a
+        @word_teaser = ""
+        word.last.size.times do
+                @word_teaser += "_ "
+        end
     end
 
     def facts
@@ -21,27 +25,48 @@ class Hangman
             ["Football club with most trophies in the world", "Al Ahly".downcase]
         ]
     end 
-    
+     def print_teaser(last_guess = nil)
+       update_teaser(last_guess) unless last_guess.nil?
+        puts @word_teaser
+    end
+
+     def update_teaser(last_guess)
+       new_teaser = @word_teaser.split
+       #replace blank values with letter if match in @word
+       new_teaser.each_with_index do |letter,index|
+           if letter == '_' && word.last[index] == last_guess
+              new_teaser[index] = last_guess
+           end
+       end
+       @word_teaser = new_teaser.join(" ")
+    end
+
     def begin
             show
             puts "Hint: #{word.first},the answer has #{word.last.size} character "
             puts "Give the answer to the hint above,remember to put a single space between names"
             puts "** a space is a character"
-            word_teaser
-            valid_move        
+            puts @word_teaser
+            valid_move    
     end
 
-    def valid_move
+    def valid_move(guess = nil)
         puts
         puts "Enter a letter"
         guess = gets.chomp
             if word.last.include?(guess)
                 puts "Correct guess!"
-            p correct_guess << guess
+                print_teaser(guess)
+                p correct_guess << guess
+                 @letters.delete(guess)
+                 
+                  valid_move
             elsif  @live > 0 && !word.last.include?(guess)
                 @live -= 1
                 puts 'Wrong guess'
                 puts "#{player}, you have #{live} attempts left, Try again"
+                puts "Hint: #{word.first},the answer has #{word.last.size} character "
+                puts "Give the answer to the hint above,remember to put a single space between names"
                 valid_move
             elsif @live == 0
                 puts "You lose, better luck next time"
@@ -49,13 +74,8 @@ class Hangman
     end
     
 
-    def word_teaser
-        word_teaser = ""
-        word.last.size.times do
-                word_teaser += "_ "
-        end
-        puts word_teaser
-    end
+   
+    
     
 
     def welcome
