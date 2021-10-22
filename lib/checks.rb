ccaa9fe183ada55a70ed96d0b29e83bc93b7fd8f
Henry Onu <onuhenry24@gmail.com>
2021-10-22 10:26:39 +0100

    Add check class

---
+++ lib/checks.rb
@@ -0,0 +1,17 @@
+class Checks
+   def name_check(player)
+       player.size >= 3
+   end
+
+   def valid_game
+     gamer = false
+        until gamer
+           puts "Input a valid name"
+           @player = gets.chomp
+           gamer = name_check(@player)
+           return @player if gamer
+           puts 'name should be at least 3 letters' if !gamer
+        end
+   
+   end
+end
